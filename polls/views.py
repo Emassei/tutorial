53b2bb1acba4c0fb42f8b5fa34c62ed74b39d08b
Ernie Massei <emasseigit@gmail.com>
2015-09-27 22:43:02 -0700

    Django part 3 'Writing more View'

--- polls/views.py
+++ polls/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello World!")
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s" % question_id)
