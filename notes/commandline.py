a386fcb88798eb717e4199653853678000d3604b
Ernie Massei <Emassei@users.noreply.github.com>
2016-03-25 09:48:10 -0700

    Delete commandline.py

--- notes/commandline.py
+++
@@ -1,5 +0,0 @@
-# This is how you download a remote database to your local
-scp cs_production:/tmp/launchdump-2015-11-19-16-53-55.sql.gz ~/Desktop/
-
-# This is how you apply a database instance to your local database
-mysql -uroot crowdstreet < launchdump-2015-11-19-16-53-55.sql
